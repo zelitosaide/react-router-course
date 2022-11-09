97eb175120186cc962f2d2a7e64fa43c5f7788fa
zelitosaide <zelitosaide83@gmail.com>
2022-11-09 09:32:14 +0200

    Auth Example

--- src/auth/layout.js
+++ src/auth/layout.js
@@ -1,7 +1,21 @@
+import { Link, Outlet } from "react-router-dom";
+
 export function Layout() {
   return (
     <div>
       <h1>Layout</h1>
+      {/* AuthStatus */}
+
+      <ul>
+        <li>
+          <Link to="/">Public Page</Link>
+        </li>
+        <li>
+          <Link to="/protected">Protected Page</Link>
+        </li>
+      </ul>
+      <h2>Layout Outlet</h2>
+      <Outlet />
     </div>
   );
 }
