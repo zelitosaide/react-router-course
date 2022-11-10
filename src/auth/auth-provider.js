43a4f3ef9c684f293448db395e0538050ab741e2
zelitosaide <zelitosaide83@gmail.com>
2022-11-10 14:24:01 +0200

    Auth Example

--- src/auth/auth-provider.js
+++ src/auth/auth-provider.js
@@ -5,9 +5,27 @@ import { AuthContext } from "./auth-context";
 export function AuthProvider({ children }) {
   const [currentUser, setCurrentUser] = useState(null);
 
-  function signin() {}
+  function signin(user, callback) {
+    fetch("..." + user)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (user) {
+        setCurrentUser(user);
+        callback();
+      });
+  }
 
-  function signout() {}
+  function signout(callback) {
+    fetch("...")
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function () {
+        setCurrentUser(null);
+        callback();
+      });
+  }
 
   return (
     <AuthContext.Provider
